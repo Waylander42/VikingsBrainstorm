91fadea0194c472b892d69fa9d0865f652c72843
Khoob <gabinmichalet@hotmail.fr>
2018-05-15 10:53:51 +0200

    Création endboardfactory
    Stocks the position of the boats at the end.

---
+++ EndBoardFactory.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Board.h"
+#include "SeaPart.h"
+
+class EndBoardFactory
+{
+public:
+	EndBoardFactory();
+	~EndBoardFactory();	
+
+	static unsigned int createEndBoard(int);
+
+	static unsigned int createEndBoard1();
+	static unsigned int createEndBoard2();
+	static unsigned int createEndBoard3();
+	static unsigned int createEndBoard4();
+	static unsigned int createEndBoard5();
+	static unsigned int createEndBoard6();
+	static unsigned int createEndBoard7();
+	static unsigned int createEndBoard8();
+	static unsigned int createEndBoard9();
+	static unsigned int createEndBoard10(); 
+	static unsigned int createEndBoard11();
+	static unsigned int createEndBoard20();
+	static unsigned int createEndBoard30();
+	static unsigned int createEndBoard40();
+	static unsigned int createEndBoard50(); 
+	static unsigned int createEndBoard60();
+};
+
