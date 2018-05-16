f42476b18358fbaf7b9c83374b34fd74e11b1225
Khoob <gabinmichalet@hotmail.fr>
2018-05-16 11:10:10 +0200

    Creation of IA

--- IA.h
+++ IA.h
@@ -1,8 +1,20 @@
 ﻿#pragma once
-class IA
+#include "Player.h"
+#include "Board.h"
+#include "Algorithm.h"
+
+
+class IA : public Player
 {
 public:
-	IA();
+	IA(Board*, unsigned int, Algorithm);
 	~IA();
+	
+	void play();
+
+private:
+	Algorithm algorithm;
 };
+
+
 
