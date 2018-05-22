18cff5279e97fc5ed311eb4fade821a903b82707
Khoob <gabinmichalet@hotmail.fr>
2018-05-22 16:53:29 +0200

    Added endboard 30-60

--- EndBoardFactory.cpp
+++ EndBoardFactory.cpp
@@ -43,8 +43,35 @@ unsigned int EndBoardFactory::createEndBoard(int number) {
 	case 28: return createEndBoard28();
 	case 29: return createEndBoard29();
 	case 30: return createEndBoard30();
+	case 31: return createEndBoard31();
+	case 32: return createEndBoard32();
+	case 33: return createEndBoard33();
+	case 34: return createEndBoard34();
+	case 35: return createEndBoard35();
+	case 36: return createEndBoard36();
+	case 37: return createEndBoard37();
+	case 38: return createEndBoard38();
+	case 39: return createEndBoard39();
 	case 40: return createEndBoard40();
+	case 41: return createEndBoard41();
+	case 42: return createEndBoard42();
+	case 43: return createEndBoard43();
+	case 44: return createEndBoard44();
+	case 45: return createEndBoard45();
+	case 46: return createEndBoard46();
+	case 47: return createEndBoard47();
+	case 48: return createEndBoard48();
+	case 49: return createEndBoard49();
 	case 50: return createEndBoard50();
+	case 51: return createEndBoard51();
+	case 52: return createEndBoard52();
+	case 53: return createEndBoard53();
+	case 54: return createEndBoard54();
+	case 55: return createEndBoard55();
+	case 56: return createEndBoard56();
+	case 57: return createEndBoard57();
+	case 58: return createEndBoard58();
+	case 59: return createEndBoard59();
 	case 60: return createEndBoard60();
 	default: NULL;
 	}
@@ -170,12 +197,120 @@ unsigned int EndBoardFactory::createEndBoard30() {
 	return 0 * 1 + 10 * 24 + 6 * 576 + 20 * 13824; //red, green, blue, yellow
 }
 
+unsigned int EndBoardFactory::createEndBoard31() {
+	return 1 * 1 + 0 * 24 + 0 * 576 + 17 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard32() {
+	return 3 * 1 + 17 * 24 + 0 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard33() {
+	return 2 * 1 + 6 * 24 + 17 * 576 + 10 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard34() {
+	return 6 * 1 + 0 * 24 + 0 * 576 + 20 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard35() {
+	return 2 * 1 + 3 * 24 + 21 * 576 + 13 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard36() {
+	return 6 * 1 + 0 * 24 + 10 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard37() {
+	return 2 * 1 + 6 * 24 + 23 * 576 + 20 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard38() {
+	return 6 * 1 + 0 * 24 + 13 * 576 + 21 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard39() {
+	return 1 * 1 + 10 * 24 + 20 * 576 + 13 * 13824; //red, green, blue, yellow
+}
+
 unsigned int EndBoardFactory::createEndBoard40() {
 	return 3 * 1 + 13 * 24 + 23 * 576 + 22 * 13824; //red, green, blue, yellow
 }
 
+unsigned int EndBoardFactory::createEndBoard41() {
+	return 0 * 1 + 6 * 24 + 20 * 576 + 10 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard42() {
+	return 0 * 1 + 3 * 24 + 23 * 576 + 20 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard43() {
+	return 1 * 1 + 0 * 24 + 20 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard44() {
+	return 2 * 1 + 3 * 24 + 23 * 576 + 10 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard45() {
+	return 0 * 1 + 21 * 24 + 13 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard46() {
+	return 6 * 1 + 13 * 24 + 17 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard47() {
+	return 3 * 1 + 13 * 24 + 22 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard48() {
+	return 1 * 1 + 2 * 24 + 17 * 576 + 3 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard49() {
+	return 2 * 1 + 3 * 24 + 17 * 576 + 13 * 13824; //red, green, blue, yellow
+}
+
 unsigned int EndBoardFactory::createEndBoard50() {
 	return 1 * 1 + 10 * 24 + 22 * 576 + 13 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard51() {
+	return 3 * 1 + 0 * 24 + 0 * 576 + 6 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard52() {
+	return 1 * 1 + 2 * 24 + 10 * 576 + 3 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard53() {
+	return 17 * 1 + 0 * 24 + 0 * 576 + 20 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard54() {
+	return 6 * 1 + 21 * 24 + 0 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard55() {
+	return 6 * 1 + 13 * 24 + 22 * 576 + 21 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard56() {
+	return 1 * 1 + 3 * 24 + 20 * 576 + 0 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard57() {
+	return 0 * 1 + 13 * 24 + 21 * 576 + 20 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard58() {
+	return 0 * 1 + 20 * 24 + 23 * 576 + 22 * 13824; //red, green, blue, yellow
+}
+
+unsigned int EndBoardFactory::createEndBoard59() {
+	return 1 * 1 + 6 * 24 + 20 * 576 + 10 * 13824; //red, green, blue, yellow
 }
 
 unsigned int EndBoardFactory::createEndBoard60() {
